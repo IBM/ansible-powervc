29410a14f7d7e6d67939cb85246916acd3e32bad
Karteesh <45362073+Karteesh@users.noreply.github.com>
2024-08-26 21:35:42 +0530

    Update unmanage_vm.py

--- plugins/modules/unmanage_vm.py
+++ plugins/modules/unmanage_vm.py
@@ -48,7 +48,7 @@ EXAMPLES = '''
 
 
 from ansible_collections.openstack.cloud.plugins.module_utils.openstack import OpenStackModule
-from ansible_collections.powervc.cloud.plugins.module_utils.crud_unmanage import unmanage_ops
+from ansible_collections.ibm.powervc.plugins.module_utils.crud_unmanage import unmanage_ops
 
 class UnmanageVMModule(OpenStackModule):
     argument_spec = dict(
