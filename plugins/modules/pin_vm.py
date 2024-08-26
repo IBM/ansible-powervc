7e5390162861d46aff8d1eafe4ec748f40ea3c39
Karteesh <45362073+Karteesh@users.noreply.github.com>
2024-08-26 21:33:44 +0530

    Update pin_vm.py

--- plugins/modules/pin_vm.py
+++ plugins/modules/pin_vm.py
@@ -48,7 +48,7 @@ EXAMPLES = '''
 
 
 from ansible_collections.openstack.cloud.plugins.module_utils.openstack import OpenStackModule
-from ansible_collections.powervc.cloud.plugins.module_utils.crud_pin import pin_ops
+from ansible_collections.ibm.powervc.plugins.module_utils.crud_pin import pin_ops
 
 class PinOpsModule(OpenStackModule):
     argument_spec = dict(
