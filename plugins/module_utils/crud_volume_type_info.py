07b597fdd07c302043b80855f9cca75b842bce63
KarteeshKumarV <vkarteesh@gmail.com>
2025-07-18 13:23:53 +0530

    Add Volume Info and VolumeType Info features
    Signed-off-by: KarteeshKumarV <vkarteesh@gmail.com>

--- plugins/module_utils/crud_volume_type_info.py
+++ plugins/module_utils/crud_volume_type_info.py
@@ -50,3 +50,4 @@ def volume_ops(mod, connectn, authtoken, tenant_id, vol_type_id):
         volume_type_url = f"{endpoint}/types"
         result = get_volume_type_details(authtoken, volume_type_url)
     return result
+
